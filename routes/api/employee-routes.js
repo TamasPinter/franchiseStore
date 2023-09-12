e83b5b62fd94e8eba06796af9cbe8c169f1fc31e
Tomas P <brolli_673@hotmail.com>
2023-09-12 13:18:24 -0400

    worked on employee routing

---
+++ routes/api/employee-routes.js
@@ -0,0 +1,78 @@
+const router = require("express").Router();
+
+const { Employee, Item, Sale } = require("../../models");
+
+// Get all employees
+router.get("/", async (req, res) => {
+  try {
+    const employeeData = await Employee.findAll({
+      include: [{ model: Item, through: Sale, as: "employee_items" }],
+    });
+    res.status(200).json(employeeData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get one employee
+router.get("/:id", async (req, res) => {
+  try {
+    const employeeData = await Employee.findByPk(req.params.id, {
+      include: [{ model: Item, through: Sale, as: "employee_items" }],
+    });
+    if (!employeeData) {
+      res.status(404).json({ message: "No employee found!" });
+    }
+    res.status(200).json(employeeData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Create a new employee
+router.post("/", async (req, res) => {
+  try {
+    const employeeData = await Employee.create(req.body);
+    res.status(200).json(employeeData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Update an employee
+router.put("/:id", async (req, res) => {
+  try {
+    const employeeData = await Employee.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!employeeData) {
+      res.status(404).json({ message: "No employee found!" });
+      return;
+    }
+    res.status(200).json(employeeData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Delete an employee
+router.delete("/:id", async (req, res) => {
+  try {
+    const employeeData = await Employee.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!employeeData) {
+      res.status(404).json({ message: "No employee found!" });
+      return;
+    }
+    res.status(200).json(employeeData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
